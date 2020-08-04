2536cf1d275984bc4584f832fcdb69e86ac236c9
peterquinn <jl.salaberri@gmail.com>
2020-08-04 17:15:34 +0200

    Version que funciona. Solo quedaria el escribir en archivo CSV la variable "archivo"

--- pandas_csv.py
+++ pandas_csv.py
@@ -27,24 +27,14 @@ for i in indices:
     print(i)
     precio_rebajado = input("Precio rebajado para el "+str(i)+": ")
     fila = archivo.iloc[i]
-    fila.at['Precio rebajado'] = str(precio_rebajado)
+    ### Para cambiar el precio rebajado a la linea i
+    archivo.at[i, 'Precio rebajado'] = str(precio_rebajado)
     #archivo.loc[i, ['Publicado', 'Precio rebajado', 'Precio normal']]
+    print('Precios Cambiados')
 
-producto = (archivo[archivo['SKU'].str.contains(number, na=False)])
-# Para sacar el index del producto cuyo CCBB es el que queremos
-nproducto = (producto.index.item())
-print(nproducto)
-# Para sacar el nombre del producto cuyo CCBB es el que queremos.
-vnombre = archivo.iloc[nproducto]['Nombre']
-print(vnombre)
-# Para sacar todas las lineas cuyo nombre sea el del producto en cuestion
-variaciones = (archivo[archivo['Nombre'].str.contains(vnombre, na=False)])
-#print(variaciones)
-# Para quitar la fila del producto madre
-variaciones = variaciones.drop([nproducto])
-# Para quedarnos solo con la parte de los precios
-acambiar = variaciones[['Nombre', 'Precio normal', 'Precio rebajado']]
-print(acambiar)
+
+print(archivo.loc[94, ['Nombre','Publicado', 'Precio rebajado', 'Precio normal']])
+
 
 # Para printar la linea 94 y con las columnas ahi marcadas
 #print(archivo.loc[nproducto, ['Nombre','Publicado', 'Precio rebajado', 'Precio normal']])
